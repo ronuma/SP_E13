fdd482e8c3da180ea4303badf9858642cf79e5c0
Alank99 <alan_hdz129@hotmail.com>
2021-12-01 10:34:06 -0600

    creacion de la clase carrito
    solo falta dos metodos y ya

--- carrito.hpp
+++ carrito.hpp
@@ -1,3 +1,42 @@
+#include <iostream>
+#include <vector>
+#include "producto.hpp"
+using namespace std;
+
 class Carrito {
+
+    private:
+        vector<Producto> _productos;
+    public:
+        Carrito(vector<Producto> productos,){
+            setProductos(productos);
+
+        }
+//---------------Sets----------------
+        void setProductos(vector<Producto> productos){
+            _productos=productos;
+        }  
+//---------------Gets----------------
+        vector<Producto> getProductos(void){
+            return _productos;
+        }
+//---------------Metodos----------------
+        void agregarProducto(Producto producto){
+            _productos.push_back(producto);
+        }
+        //calcule el total con las propiedad de los precion de nuestra lista de productos
+        double calcularTotal(vector<Producto> _productos){
+          double total = 0;
+          for (int i = 0; i < _productos.size(); i++)
+          {
+              total = total + _productos[i].getPrecio();
+          }
+            return total;
+        }
+        void impimirCarrito(void){
+
+        }
+        
+
 
 };
