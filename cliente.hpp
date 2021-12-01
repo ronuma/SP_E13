2f2bdd147556341093360af1312b62b514e15b07
Alank99 <alan_hdz129@hotmail.com>
2021-12-01 09:45:51 -0600

    creacion de cliente
    cree toda la clase cliente

--- cliente.hpp
+++ cliente.hpp
@@ -1,3 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
 class Cliente {
+
+    private:
+        string _Email, _Direccion;
+    
+    public:
+//-----------Constructores----------
+        Cliente(void){
+            _Email = "";
+         _Direccion = "";
+        }
+
+        Cliente(string Email, string Direccion ){
+            setEmail(Email);
+            setDireccion(Direccion);
+        }
+//-------------Sets-----------------
+        void setEmail(string Email){
+            _Email = Email;
+        }
+        void setDireccion(string Direccion){
+            _Direccion = Direccion;
+        }
+        
+//-------------Gets-----------------
+        string getEmail(void){
+            return _Email;
+        }
+
+        string getDireccion(void){
+            return _Direccion;
+        }
+//-------------Metodos-----------------
+        
+        void imprimirCliente(void){
+            cout<<"Su email es"<<_Email<<endl;
+            cout<<" Direccion es"<<_Direccion<<endl;
+        }
+
 
 };
