7ad7fbe557742504c820066b250c2e3c50fc9c97
ronuma <ronum.00@gmail.com>
2021-12-02 18:57:58 -0600

    pruebas
    Agregué casi todas las pruebas, faltaría lo que tiene que ver con el carrito porque es probable que convenga declararlo como una propiedad de la tienda, o sea incluirlo ahi, ya que si lo tenemos como un objeto en la tienda se sobreescribe la declaracion de producto porque está incluido en Tienda y en Carrito al mismo tiempo

--- main.cpp
+++ main.cpp
@@ -5,7 +5,42 @@
 using namespace std;
 
 int main(void) {
+    int productoSeleccionado, cantidad, continuarInput;
+    bool continuar = true;
+    bool agregarAlCarrito;
+
     Tienda tienda = Tienda(Cliente("rodrigo@tec.mx", "Fuente de la calle 3"));
     tienda.mostrarCatalogo();
+    vector<Producto> catalogo = tienda.getCatalogo();
     tienda.getCliente().imprimir();
+
+    while(continuar == true) {
+        cout << "Ingrese el indice del producto que desea agregar al carrito: ";
+        cin >> productoSeleccionado;
+        cout << endl;
+
+        cout << "Seleccion: " << endl;
+        catalogo[productoSeleccionado].imprimir();
+
+        cout << "Indique la cantidad que desea agregar al carrito ";
+        cout << "(" << catalogo[productoSeleccionado].enInventario;
+        cout << " en inventario): ";
+        cin >> cantidad;
+        cout << endl;
+
+        cout << "Desea agregar otro producto al carrito (0) o pagar (1)?: ";
+        cin >> continuarInput;
+        cout << endl;
+        
+        //EVALUAR SI LA SELECCION DE USUARIO PERMITE SACAR DEL INVENTARIO
+
+        if(continuarInput == 0) {
+            continuar = true;
+        } else if (continuarInput == 1) {
+            //MOSTRAR EL CARRITO
+            //MOSTRAR EL TOTAL A PAGAR Y LA INFORMACION DEL CLIENTE
+            continuar = false;
+        }
+        
+    }
 }
